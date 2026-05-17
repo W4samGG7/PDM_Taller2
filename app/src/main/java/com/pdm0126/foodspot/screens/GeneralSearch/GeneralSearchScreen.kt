3b52ee439433ccf5a2b075c237ecdcaf2aca5856
W4samGG7 <00097524@uca.edu.sv>
2026-05-17 13:16:19 -0600

    feat: Agregar mensaje de no se encontraron resultados para el uusario en Busqueda General

--- app/src/main/java/com/pdm0126/foodspot/screens/GeneralSearch/GeneralSearchScreen.kt
+++ app/src/main/java/com/pdm0126/foodspot/screens/GeneralSearch/GeneralSearchScreen.kt
@@ -1,9 +1,16 @@
 package com.pdm0126.foodspot.screens.GeneralSearch
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,15 +23,20 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pdm0126.foodspot.components.SearchBar
 import com.pdm0126.foodspot.components.TopBar
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import com.pdm0126.foodspot.components.RestaurantCard
 
 @Composable
 fun GeneralSearchScreen(
     modifier: Modifier = Modifier,
-    navigateToRestaurantList:() -> Unit,
+    navigateToRestaurantList: () -> Unit,
     viewModel: GeneralSearchViewModel = viewModel()
-){
+) {
     LaunchedEffect(Unit) {
         viewModel.clearSearch()
     }
@@ -35,23 +47,52 @@ fun GeneralSearchScreen(
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     ) {
-        TopBar(title = "Buscar", showSearch = false, onSearch = {}, showBack = true, onBack = {navigateToRestaurantList()}  )
+        TopBar(
+            title = "Buscar",
+            showSearch = false,
+            onSearch = {},
+            showBack = true,
+            onBack = { navigateToRestaurantList() })
         SearchBar(searchQuery = searchValue, onSearch = { search ->
-            viewModel.searchOnRestaurants(search)})
-        Text(
-            text = "${restaurantsSearched.size} resultados encontrados"
-        )
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(15.dp),
-            contentPadding = PaddingValues(20.dp)
-        ) {
-            items(restaurantsSearched){ restaurantFounded ->
-                RestaurantCard(
-                    restaurant = restaurantFounded
+            viewModel.searchOnRestaurants(search)
+        })
+
+        if(searchValue.isEmpty()) {
+
+        } else if (restaurantsSearched.isEmpty() ) {
+            Column (
+                modifier = modifier.height(150.dp).width(250.dp).border(width = 2.dp,color = Color.LightGray, shape = RectangleShape),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "busqueda no encontrada",
+                    tint = Color.LightGray,
+                    modifier = Modifier
+                        .size(50.dp)
                 )
+                Text(
+                    text = "No se encontraron resultados",
+                    color = Color.LightGray
+                )
+            }
+        } else {
+            Text(
+                text = "${restaurantsSearched.size} resultados encontrados"
+            )
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(15.dp),
+                contentPadding = PaddingValues(20.dp)
+            ) {
+                items(restaurantsSearched) { restaurantFounded ->
+                    RestaurantCard(
+                        restaurant = restaurantFounded
+                    )
+                }
             }
         }
     }
